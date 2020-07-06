0c3fc3c9863582716671329502013f53bbe33a59
Tyson Lackey <lackeyt90@gmail.com>
2020-07-06 15:26:00 -0700

    create id assignment test refactor previous tests

--- __tests__/aggregateEvents.test.js
+++ __tests__/aggregateEvents.test.js
@@ -1,18 +1,27 @@
 import {AggregateEvents} from './../src/aggregateEvents.js';
+import {Event} from './../src/eventObject.js'
 
 describe('Aggregate Events and related object methods', ()=>{
   let reusableEventIndex;
+  let reusableEvent;
 
   beforeEach(()=>{
     reusableEventIndex = new AggregateEvents;
+    reusableEvent = new Event("This Event", "Tyson", "description of event", "Portland, OR", "07/06/2020 12:00PM");
   })
 
   test('create new AggregateEvents object', ()=>{
     expect(reusableEventIndex.events).toEqual([]);
+    expect(reusableEventIndex.currentId).toEqual(0)
   })
 
-  test('add recipe to AggregateRecipe object', ()=>{
-    reusableEventIndex.addEvent('testEvent');
-    expect(reusableEventIndex.events).toEqual(['testEvent']);
+  test('add event to AggregateEvents object', ()=>{
+    reusableEventIndex.addEvent(reusableEvent);
+    expect(reusableEventIndex.events).toEqual([reusableEvent]);
+  })
+
+  test('assigns id to event object when added to AggregateEvents', ()=>{
+    reusableEventIndex.addEvent(reusableEvent);
+    expect(reusableEvent.id).toEqual(1);
   })
 })
--- src/aggregateEvents.js
+++ src/aggregateEvents.js
@@ -1,9 +1,16 @@
 export class AggregateEvents {
   constructor() {
     this.events = [];
+    this.currentId = 0;
   }
 
   addEvent(event) {
+    event.id = this.assignEventId()
     this.events.push(event);
+  }
+
+  assignEventId(){
+    this.currentId += 1;
+    return this.currentId;
   }
 }

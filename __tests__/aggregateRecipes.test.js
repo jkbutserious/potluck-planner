98cc84c9d86906031dc92f39144c8d648116c033
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-06 11:56:07 -0700

    save aggregateRecipe test

--- __tests__/aggregateRecipes.test.js
+++ __tests__/aggregateRecipes.test.js
@@ -2,13 +2,19 @@ import { AggregateRecipes } from './../src/aggregateRecipes.js';
 
 describe('instantiate new AggregateRecipe object', () => {
   let appRecipeIndex;
+  let recipe;
 
   beforeEach(() => {
     appRecipeIndex = new AggregateRecipes();
+    recipe = 'apple pie'
   });
 
-  test('should correctly instantiate new Aggregate Recipe object', () => {
+  test('should correctly instantiate new AggregateRecipe object', () => {
     expect(appRecipeIndex.recipes).toEqual([]);
+  });
+
+  test('should correctly add recipe to AggregateRecipe object', () => {
+    expect(appRecipeIndex.addRecipe(recipe)).toEqual(['apple pie']);
   });
 
 });

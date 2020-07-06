34449abaacd6f190000303055cc8dba6f8a4fcf4
Tyson Lackey <lackeyt90@gmail.com>
2020-07-06 16:08:58 -0700

    create assignUserId test, red light

--- __tests__/aggregateUsers.test.js
+++ __tests__/aggregateUsers.test.js
@@ -1,12 +1,13 @@
 import { AggregateUsers } from './../src/aggregateUsers.js';
+import { User } from '../src/user-object.js';
 
 describe('instantiate new AggregateUser object and add users', () => {
   let userIndex;
-  let user;
+  let reusableUser;
 
   beforeEach(() => {
     userIndex = new AggregateUsers();
-    user = 'Hannah'
+    reusableUser = new User('Tyson', 'testAboutMe', 'Portland')
   });
 
   test('should correctly instantiate new AggregateRecipe object', () => {
@@ -14,7 +15,12 @@ describe('instantiate new AggregateUser object and add users', () => {
   });
 
   test('should correctly add user to AggregateUsers object', () => {
-    userIndex.addUser(user);
-    expect(userIndex.users).toEqual(['Hannah']);
+    userIndex.addUser(reusableUser);
+    expect(userIndex.users).toEqual([reusableUser]);
   });
+
+  test('assigns id to user object when added to AggregateUsers', ()=>{
+    userIndex.assignUserId(reusableUser);
+    expect(reusableUser.id).toEqual(1);
+  })
 });
--- src/aggregateUsers.js
+++ src/aggregateUsers.js
@@ -4,6 +4,12 @@ export class AggregateUsers {
   }
 
   addUser(user) {
+    user.id = this.assignUserId();
     this.users.push(user);
+  }
+
+  assignUserId(user){
+    this.currentId += 1;
+    return this.currentId;
   }
 }

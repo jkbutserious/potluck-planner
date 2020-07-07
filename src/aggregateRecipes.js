90d122cffd0784b5ac586dcba4beaaa864ed5d71
Tyson Lackey <lackeyt90@gmail.com>
2020-07-07 10:43:56 -0700

    green light assign recipe id test

--- src/aggregateRecipes.js
+++ src/aggregateRecipes.js
@@ -1,13 +1,16 @@
 export class AggregateRecipes {
   constructor() {
     this.recipes = [];
+    this.currentId = 0;
   }
 
   addRecipe(recipe) {
+    recipe.id = this.assignRecipeId();
     this.recipes.push(recipe);
   }
 
   assignRecipeId(){
-    
+    this.currentId += 1;
+    return this.currentId;
   }
 }

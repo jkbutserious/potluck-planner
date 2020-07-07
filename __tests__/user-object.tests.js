e1a445ca774536e8600157ddcaa7c2c6e6bb76fc
Tyson Lackey <lackeyt90@gmail.com>
2020-07-07 11:49:31 -0700

    create add recipe to user test, red light

--- __tests__/user-object.tests.js
+++ __tests__/user-object.tests.js
@@ -1,10 +1,15 @@
-import { User } from '../src/user-object.js'
+import { User } from '../src/user-object.js';
+import { Recipe } from '../src/recipe-object.js';
+import { AggregateRecipes } from '../src/aggregateRecipes.js';
+
 describe('should include all tests related to the user object', () => {
   let userObject;
+  let recipeIndex;
 
   beforeEach(() => {
     const description = "Likes long walks on the beach"
     userObject = new User("Kyle", description, "Portland, OR");
+    recipeIndex = new AggregateRecipes();
   });
 
   test('should properly instantiate new user object', () => {
@@ -12,5 +17,12 @@ describe('should include all tests related to the user object', () => {
     expect(userObject.recipes).toEqual([]);
     expect(userObject.aboutMe).toEqual("Likes long walks on the beach");
     expect(userObject.location).toEqual("Portland, OR");
+  });
+
+  test('add a new recipe to the user object', () => {
+    let recipe = new Recipe("potluck");
+    recipeIndex.addRecipe(recipe);
+    userObject.addRecipe(recipe);
+    expect(userObject.recipes).toEqual([recipe]);
   });
 })
--- src/user-object.js
+++ src/user-object.js
@@ -5,4 +5,8 @@ export class User {
     this.aboutMe = aboutMe;
     this.location = location;
   }
+
+  addRecipe(recipe){
+
+  }
 }
